ae3c937d983e6306fceb52e34c4b58e21eced033
Crawford <crawford.collins@gmail.com>
2019-11-13 21:35:38 -0600

    wednes work complete

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.append('../queue_and_stack')
+sys.path.append("../queue_and_stack")
 from dll_queue import Queue
 from dll_stack import Stack
 
@@ -12,22 +12,49 @@ class BinarySearchTree:
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
+        if value < self.value:
+            if self.left is None:
+                self.left = BinarySearchTree(value)
+            else:
+                self.left.insert(value)
+
+        elif value > self.value:
+            if self.right is None:
+                self.right = BinarySearchTree(value)
+            else:
+                self.right.insert(value)
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+        if target == self.value:
+            return True
+        elif target < self.value:
+            try:
+                return self.left.contains(target)
+            except:
+                return False
+        elif target > self.value:
+            try:
+                return self.right.contains(target)
+            except:
+                return False
 
     # Return the maximum value found in the tree
     def get_max(self):
-        pass
+        try:
+            return self.right.get_max()
+        except:
+            return self.value
 
     # Call the function `cb` on the value of each node
     # You may use a recursive or iterative approach
     def for_each(self, cb):
-        pass
-
+        cb(self.value)
+        if self.right is not None:
+            self.right.for_each(cb)
+        if self.left is not None:
+            self.left.for_each(cb)
     # DAY 2 Project -----------------------
 
     # Print all the values in order from low to high
